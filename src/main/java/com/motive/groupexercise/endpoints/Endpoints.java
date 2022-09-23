502a59929676becdfc8b8d976d29712385fc4012
R Keegan Wright <62271215+Rennie-K-Wright@users.noreply.github.com>
2022-09-22 18:26:33 -0700

    Fixed HTML added a bit of CSS
    added @RequestMapping to Content Controller, Endpoints, refractored greeting.html to products.html, moved products from templates to static=

--- src/main/java/com/motive/groupexercise/endpoints/Endpoints.java
+++ src/main/java/com/motive/groupexercise/endpoints/Endpoints.java
@@ -5,12 +5,14 @@ import com.motive.groupexercise.domain.service.ProductService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 import reactor.core.publisher.Mono;
 
 import java.util.List;
 
 @RestController
+@RequestMapping(value="/pain")
 public class Endpoints {
     private ProductService productService;
 
